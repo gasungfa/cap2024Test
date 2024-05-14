4d0eac8876596f030308f7eadf6fc661265280d8
나상연 <58220151+ense333@users.noreply.github.com>
2024-05-14 20:41:08 +0900

    테스트 프로젝트 시작

---
+++ cap2024test/src/main/java/com/example/cap2024test/Cap2024testApplication.java
@@ -0,0 +1,13 @@
+package com.example.cap2024test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap2024testApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap2024testApplication.class, args);
+	}
+
+}
---
+++ cap2024test/src/test/java/com/example/cap2024test/Cap2024testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cap2024test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cap2024testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
